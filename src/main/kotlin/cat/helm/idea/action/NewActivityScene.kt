102394793d04bf3dfb0ad0e0c03ee6d2f6cfd937
Héctor Manrique Garcia <hmanrique32@gmail.com>
2017-07-26 12:28:33 +0200

    add checkbox in InputDialog to question the user is the activity is main or not to change add intent filter action in manifest

--- src/main/kotlin/cat/helm/idea/action/NewActivityScene.kt
+++ src/main/kotlin/cat/helm/idea/action/NewActivityScene.kt
@@ -1,32 +1,16 @@
 import cat.helm.idea.template.Template
-import com.android.ide.common.rendering.LayoutLibrary
-import com.android.ide.common.resources.configuration.FolderConfiguration
-import com.android.resources.ResourceFolderType
-import com.android.resources.ResourceType
-import com.android.sdklib.IAndroidTarget
-import com.android.tools.idea.editors.strings.StringsWriteUtils
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.LangDataKeys
 import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.roots.ProjectRootManager
 import com.intellij.openapi.ui.Messages
-import com.intellij.openapi.util.io.FileUtil
-import com.intellij.openapi.vfs.LocalFileSystem
 import com.intellij.openapi.vfs.ReadonlyStatusHandler
 import com.intellij.psi.*
-import com.intellij.psi.impl.file.PsiJavaDirectoryImpl
 import com.intellij.psi.search.GlobalSearchScope
 import org.jetbrains.android.facet.AndroidFacet
 import org.jetbrains.android.dom.manifest.*
-import org.jetbrains.android.formatter.AndroidXmlCodeStyleSettings
-import org.jetbrains.android.uipreview.LayoutLibraryLoader
-import org.jetbrains.android.util.AndroidResourceUtil
 import org.jetbrains.android.util.AndroidUtils
-import org.jetbrains.jps.android.builder.AndroidResourceCachingBuildTarget
-import org.jetbrains.jps.incremental.artifacts.builders.LayoutElementBuilderService
-import org.jetbrains.kotlin.idea.core.getPackage
 import org.jetbrains.kotlin.idea.decompiler.navigation.SourceNavigationHelper
 import org.jetbrains.kotlin.psi.KtClass
 import org.jetbrains.kotlin.psi.KtFile
@@ -38,13 +22,15 @@ import org.jetbrains.kotlin.psi.KtFile
 class NewActivityScene : AnAction() {
 
     override fun actionPerformed(actionEvent: AnActionEvent) {
-        val sceneName = Messages.showInputDialog("Enter new scene name", "New Scene", null)
-        sceneName?.let {
+        val dialogValues = Messages.showInputDialogWithCheckBox("Enter new scene name", "New Scene", "Es main activity?", false, true, null, null, null)
+        val sceneName = dialogValues.first
+        val isMainActivity = dialogValues.second
+        sceneName.let {
 
             val project = actionEvent.project
             project?.let {
 
-                val correctPathName = sceneName.substring(0,1).toLowerCase()+sceneName.substring(1)
+                val correctPathName = sceneName.substring(0, 1).toLowerCase() + sceneName.substring(1)
 
 
                 WriteCommandAction.runWriteCommandAction(project) {
@@ -52,15 +38,15 @@ class NewActivityScene : AnAction() {
                     val destinationDirectory = actionEvent.getData(LangDataKeys.PSI_ELEMENT) as PsiDirectory
                     val directory = createDirectory(destinationDirectory, correctPathName)
 
-                    if(directory!=null){
+                    if (directory != null) {
 
-                        val correctFileName = sceneName.substring(0,1).toUpperCase()+sceneName.substring(1)
+                        val correctFileName = sceneName.substring(0, 1).toUpperCase() + sceneName.substring(1)
 
                         val activityFile = createSceneFiles(correctFileName, project, directory)
-                        addActivityEntryToManifest(actionEvent, project, activityFile)
+                        addActivityEntryToManifest(actionEvent, project, activityFile, isMainActivity)
                     } else {
 
-                        Messages.showMessageDialog("Ya existe una carpeta con el mismo nombre.","Information",Messages.getErrorIcon())
+                        Messages.showMessageDialog("Ya existe una carpeta con el mismo nombre.", "Information", Messages.getErrorIcon())
                     }
 
 
@@ -70,7 +56,7 @@ class NewActivityScene : AnAction() {
         }
     }
 
-    private fun addActivityEntryToManifest(actionEvent: AnActionEvent, project: Project?, activityFile: PsiElement) {
+    private fun addActivityEntryToManifest(actionEvent: AnActionEvent, project: Project?, activityFile: PsiElement, isMainActivity: Boolean) {
         //Todo Show Android facet error
         val facet = AndroidFacet.getInstance(actionEvent.getData(LangDataKeys.PSI_ELEMENT)!!)
 
@@ -79,23 +65,32 @@ class NewActivityScene : AnAction() {
                 !ReadonlyStatusHandler.ensureFilesWritable(facet.module.project, manifest)) {
             throw Exception()
         }
-        val manifestDoom = AndroidUtils.loadDomElement(facet.module, manifest, Manifest::class.java)
-        val application = manifestDoom?.application
+        val manifestDom = AndroidUtils.loadDomElement(facet.module, manifest, Manifest::class.java)
+        val application = manifestDom?.application
         WriteCommandAction.runWriteCommandAction(project) {
             val activity = application!!.addActivity()
             activity.activityClass.value = SourceNavigationHelper.getOriginalPsiClassOrCreateLightClass((activityFile as KtFile).declarations[0] as KtClass)
+            if (isMainActivity) {
+                val filter = activity.addIntentFilter()
+                val action = filter.addAction()
+                action.name.value = AndroidUtils.LAUNCH_ACTION_NAME
+                val category = filter.addCategory()
+                category.name.value = AndroidUtils.LAUNCH_CATEGORY_NAME
+            }
         }
     }
 
     private fun createSceneFiles(sceneName: String, project: Project, destinationDirectory: PsiDirectory): PsiElement {
 
 
-        val correctLayoutName = sceneName.substring(0,1).toLowerCase()+sceneName.substring(1)
+        val correctLayoutName = sceneName.substring(0, 1).toLowerCase() + sceneName.substring(1)
         val destinationDirectoryLayout = JavaPsiFacade.getInstance(project).findPackage("layout")?.getDirectories(GlobalSearchScope.projectScope(project))?.get(0)
         val layoutTemplate = Template.Layout(correctLayoutName)
-        layoutTemplate.createTemplate(project,destinationDirectoryLayout!!)
+        layoutTemplate.createTemplate(project, destinationDirectoryLayout!!)
 
-        val activityTemplate = Template.Activity(sceneName,destinationDirectory)
+        //AndroidResourceUtil.createFileResource()
+
+        val activityTemplate = Template.Activity(sceneName, destinationDirectory)
         val activityFile = activityTemplate.createTemplate(project, destinationDirectory)
 
         val presenterTemplate = Template.Presenter(sceneName)
